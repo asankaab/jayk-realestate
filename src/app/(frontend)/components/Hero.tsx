dc9ba4887be6aa7eb8f84f6c97a9853bcbbd619c
asankaab <asanka.abewickrama@gmail.com>
2026-02-26 15:54:56 +0530

    hero right column

--- src/app/(frontend)/components/Hero.tsx
+++ src/app/(frontend)/components/Hero.tsx
@@ -1,4 +1,5 @@
-import { Heading1, Heading4 } from './Text/Text'
+import { Building2, Home, LandPlot, Store } from 'lucide-react'
+import { Heading1, Heading4, Small } from './Text/Text'
 import { SearchBar } from './SearchBar'
 import styles from './Hero.module.css'
 import Button from './Button'
@@ -8,13 +9,10 @@ export const Hero = () => {
     <div className={styles.heroContainer}>
       <div className={styles.hero + ' wrapper'}>
         <div className={styles.leftColumn}>
-          <Heading1>
-            Unlock the door to <br />
-            your future.
-          </Heading1>
+          <Heading1>Unlock the door to your future.</Heading1>
           <Heading4>
-            We are a full-service real estate company, and we can help <br />
-            you find the perfect place to live.
+            We are a full-service real estate company, and we can help you find the perfect place to
+            live.
           </Heading4>
           <div className={styles.buttons}>
             <Button href="properties" fill="outlined">
@@ -26,6 +24,26 @@ export const Hero = () => {
           </div>
         </div>
         <div className={styles.rightColumn}>
+          <div className={styles.propertyTypes}>
+            <div className={styles.propertyTypeBoxes}>
+              <div className={styles.propertyTypeBox}>
+                <Home />
+                <Small>Houses</Small>
+              </div>
+              <div className={styles.propertyTypeBox}>
+                <LandPlot />
+                <Small>Lands</Small>
+              </div>
+              <div className={styles.propertyTypeBox}>
+                <Building2 />
+                <Small>Offices</Small>
+              </div>
+              <div className={styles.propertyTypeBox}>
+                <Store />
+                <Small>Stores</Small>
+              </div>
+            </div>
+          </div>
           <SearchBar />
         </div>
       </div>
