6638e23527a326e569b7b90e8a2ed88c3c23e7c1
asanka c abewickrama <asanka.abewickrama@gmail.com>
2026-04-24 23:59:04 +0530

    Update ClerkPayloadProvider.tsx

--- src/app/(payload)/admin/components/ClerkPayloadProvider.tsx
+++ src/app/(payload)/admin/components/ClerkPayloadProvider.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
 
 export const ClerkPayloadProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const disableDevelopmentMode = process.env.NEXT_PUBLIC_DISABLE_DEVELOPMENT_MODE
+  const disableDevelopmentMode = process.env.NEXT_PUBLIC_DISABLE_CLERK_DEV_MODE
 
   return (
     <ClerkProvider
