9b3c65f01654fd40411b937fa0a722d601b89450
asanka c abewickrama <asanka.abewickrama@gmail.com>
2026-04-28 10:17:06 +0530

    Update Navbar.tsx dashboard link to close navmenu

--- src/app/(frontend)/components/Navbar.tsx
+++ src/app/(frontend)/components/Navbar.tsx
@@ -173,7 +173,7 @@ const Navbar = () => {
                       size="small"
                       color="primary"
                       href="/dashboard"
-                      onClick={toggleMenu}
+                      onClick={()=>toggleMenu()}
                     >
                       Dashboard
                     </Button>
